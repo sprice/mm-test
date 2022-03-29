e32f9fb8832c5638b3dd59e4e5835015a39966d7
Shawn Price <shawnp@gmail.com>
2022-03-29 11:33:35 -0700

    MVP wagmi implementation

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,48 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+
+import { Provider, chain, defaultChains } from "wagmi";
+import { InjectedConnector } from "wagmi/connectors/injected";
+import { WalletConnectConnector } from "wagmi/connectors/walletConnect";
+import { WalletLinkConnector } from "wagmi/connectors/walletLink";
+
+// API key for Ethereum node
+// Two popular services are Infura (infura.io) and Alchemy (alchemy.com)
+const infuraId = process.env.NEXT_PUBLIC_INFURA_ID;
+
+// Chains for connectors to support
+const chains = defaultChains;
+
+// Set up connectors
+const connectors = ({ chainId }) => {
+  const rpcUrl =
+    chains.find((x) => x.id === chainId)?.rpcUrls?.[0] ??
+    chain.mainnet.rpcUrls[0];
+  return [
+    new InjectedConnector({
+      chains,
+      options: { shimDisconnect: true },
+    }),
+    new WalletConnectConnector({
+      options: {
+        infuraId,
+        qrcode: true,
+      },
+    }),
+    new WalletLinkConnector({
+      options: {
+        appName: "My wagmi app",
+        jsonRpcUrl: `${rpcUrl}/${infuraId}`,
+      },
+    }),
+  ];
+};
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <Provider autoConnect connectors={connectors}>
+      <Component {...pageProps} />
+    </Provider>
+  );
 }
 
-export default MyApp
+export default MyApp;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,50 @@
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import React from "react";
+import { useAccount, useConnect } from "wagmi";
+
+const useIsMounted = () => {
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
+  return mounted;
+};
 
 export default function Home() {
-  return <p>hello</p>;
+  const [{ data: connectData, error: connectError }, connect] = useConnect();
+  const [{ data: accountData }, disconnect] = useAccount({
+    fetchEns: true,
+  });
+
+  const isMounted = useIsMounted();
+
+  return (
+    <div>
+      <div>
+        {connectData.connectors.map((connector) => (
+          <button
+            disabled={isMounted && !connector.ready}
+            key={connector.id}
+            onClick={() => connect(connector)}
+          >
+            {isMounted && connector.name}
+            {isMounted && !connector.ready && " (unsupported)"}
+          </button>
+        ))}
+
+        {connectError && (
+          <div>{connectError?.message ?? "Failed to connect"}</div>
+        )}
+      </div>
+      {accountData && (
+        <div>
+          <img src={accountData.ens?.avatar} alt="ENS Avatar" />
+          <div>
+            {accountData.ens?.name
+              ? `${accountData.ens?.name} (${accountData.address})`
+              : accountData.address}
+          </div>
+          <div>Connected to {accountData.connector.name}</div>
+          <button onClick={disconnect}>Disconnect</button>
+        </div>
+      )}
+    </div>
+  );
 }
